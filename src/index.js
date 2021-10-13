89b51102801ed7316e64546bf9a4c4b12e09cbac
Marc Raphael <marcraphael979@gmail.com>
2021-10-13 01:25:58 +0100

    src:  Create a module for the leaderboard, import it,  and delete it from html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-import "./styles/style.scss";
+import './styles/style.scss';
+import listElements from './lists.js';
+
+const container = document.querySelector('.recent-scores');
+const list = listElements();
+
+container.appendChild(list);

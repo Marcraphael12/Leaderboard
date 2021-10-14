f85b3e09dee64d35872dba128c9e694654884dbc
Marc Raphael <marcraphael979@gmail.com>
2021-10-14 12:19:50 +0100

    JS: Reset script code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,1 @@
 import './styles/style.scss';
-import listElements from './lists.js';
-
-const container = document.querySelector('.recent-scores');
-const list = listElements();
-
-container.appendChild(list);

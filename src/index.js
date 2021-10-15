1575b301ba97bb1b9681fe51553d55742e3a8fdf
Marc Raphael <marcraphael979@gmail.com>
2021-10-15 01:09:39 +0100

    JS: Create a function to display the users scores

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import './styles/style.scss';
 
-const API = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/hhuU7Jwb1hkX96Lmktcc/scores/'; // The API link
+const API = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/hhuU7Jwb1hkX96Lmktcc/scores/';
+
+const request = new XMLHttpRequest(); // The request type
 
 const addButton = document.querySelector('.add-list'); // Submit the form and add a score to the score list
+const scoresContainer = document.querySelector('.scores-container'); // Score list container
 
-// we need to send the data to the API using a function
-const sendTheData = () => {
-  // we need our inputs values
-  const name = document.querySelector('.name').value;
-  const score = document.querySelector('.score').value;
+const displayTheUsersScore = (user) => {
+  // for each score list we need a li and some spans to get the score and the name
+  user.forEach((score) => {
+    const scoreList = document.createElement('li');
+    scoreList.classList.add('scores', 'flex', 'flex-jc-sb,', 'flex-ai-c');
+    scoreList.innerHTML = `<span class="score-name">${score.user}</span> <span class="score-score">${score.score}</span>`;
+
+    // The we add the user's score to the score list
+    scoresContainer.appendChild(scoreList);
+  });
 };
 
-// past them as argument to the body
-// when the user submit the form the data are sended to the API
